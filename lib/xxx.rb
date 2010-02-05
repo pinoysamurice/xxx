26dc56672dbc75ae21347a9d41d31d6212a6c830
Jakub Kuźma <qoobaa+github@gmail.com>
2010-02-05 18:07:11 +0100

    added channels

--- lib/xxx.rb
+++ lib/xxx.rb
@@ -6,9 +6,26 @@ require "xxx/youporn"
 require "xxx/szukaj_cipki"
 
 module Xxx
-  CHANNELS = [Youporn, SzukajCipki]
-  def self.watch_porn
-    channel = CHANNELS.sort_by { Kernel.rand }.first
-    Launchy.open(channel.new.latest)
+  CHANNEL_NAMES = [:youporn, :szukajcipki]
+
+  def open_channel(channel_name)
+    case channel
+    when :youporn
+      Youporn.new
+    when :szukajcipki
+      SzukajCipki.new
+    end
+  end
+
+  def self.watch_porn(channel_name, command = "latest")
+    channel_name ||= CHANNEL_NAMES.sort_by { rand }.first
+    channel = open_channel(channel_name)
+
+    if channel.respond_to?(command)
+      uri = channel.send(command)
+      Launchy.open(uri)
+    else
+      raise ArgumentError, "unknown command: #{command}"
+    end
   end
 end
