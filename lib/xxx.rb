b5398336388ae20a372375e0686478413fa700cc
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2010-02-05 17:57:23 +0100

    working version

--- lib/xxx.rb
+++ lib/xxx.rb
@@ -4,3 +4,11 @@ require "launchy"
 
 require "xxx/youporn"
 require "xxx/szukaj_cipki"
+
+module Xxx
+  CHANNELS = [Youporn, SzukajCipki]
+  def self.watch_porn
+    channel = CHANNELS.sort_by { Kernel.rand }.first
+    Launchy.open(channel.new.latest)
+  end
+end
